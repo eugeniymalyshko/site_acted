3dd6a29614f30ac8fb793b39d3fefab14832efc8
Yevhenii Malyshko <yevheniimalushko@zoho.com>
2021-02-02 13:16:03 +0200

    Structure of the site

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//===================================== IBG ==============================================
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if(ibg[i].querySelector('img')){
+			ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+		}
+	}
+}
+ibg();
+//========================================================================================
+//================================= BURGER MENU ==========================================
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector('.header__burger').onclick = function (event) {
+		document.querySelector('.header__burger').classList.toggle('active');
+		document.querySelector('.header__menu').classList.toggle('active');
+		document.querySelector('body').classList.toggle('lock');
+    }
+});
+//========================================================================================

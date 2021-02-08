002d101c826985170f7683c4be2e58b36d6fb45d
Yevhenii Malyshko <yevheniimalushko@zoho.com>
2021-02-08 22:31:23 +0200

    added burger

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ ibg();
 document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector('.header__burger').onclick = function (event) {
 		document.querySelector('.header__burger').classList.toggle('active');
-		document.querySelector('.header__menu').classList.toggle('active');
+		document.querySelector('.header__menu_hidden').classList.toggle('active');
+		document.querySelector('.top-bar').classList.toggle('hidden');
 		document.querySelector('body').classList.toggle('lock');
     }
 });
